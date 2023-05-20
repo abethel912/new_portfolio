9cec25875f8b85bf9d0433bfd38179116acfdbb2
abethel912 <abethel180@gmail.com>
2023-05-20 17:36:50 -0400

    Contact component created

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Skills from './components/Skills'
 import Work from "./components/Work";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <div>
       <Navbar /> 
+      <Contact />
       <Home />
       <About />
       <Skills />
